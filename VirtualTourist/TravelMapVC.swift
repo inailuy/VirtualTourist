7e52c518ae4064f97ab6734a3a6fe7df49ac531c
iNailuY <inailuy@me.com>
2016-04-19 19:27:09 -0500

    Implemented interaction logic for TravelMapVC

--- VirtualTourist/TravelMapVC.swift
+++ VirtualTourist/TravelMapVC.swift
@@ -10,16 +10,41 @@ import MapKit
 
 class TravelMapVC: UIViewController, MKMapViewDelegate {
     
+    @IBOutlet weak var deletePinLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var deletePinState = true
+    
+    @IBAction func longPressedGesture(sender: UILongPressGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.Ended {
+            let touchPoint = sender.locationInView(mapView)
+            let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            
+            mapView.addAnnotation(annotation)
+            print(mapView.annotations.count)
+        }
+    }
+    //MARK: MapView Delegate
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        performSegueWithIdentifier("photoSegue", sender: nil)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func barButtonPressed(sender: AnyObject) {
+        deleteLabelAnimation(!deletePinState)
     }
     
+    func deleteLabelAnimation(state:Bool) {
+        var frame = self.deletePinLabel.frame
+        if state == true {
+            frame.origin.y = self.mapView.frame.size.height - self.deletePinLabel.frame.size.height
+        } else {
+            frame.origin.y = self.view.frame.size.height
+        }
+        UIView.animateWithDuration(0.5, animations: {
+            self.deletePinLabel.frame = frame
+        })
+        self.deletePinState = state
+    }
     
 }
